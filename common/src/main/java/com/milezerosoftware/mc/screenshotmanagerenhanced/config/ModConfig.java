44c840990c00772735d95796289bc1ac93b2aa9c
scriptmunkeeofficial <scriptmunkee@gmail.com>
2026-01-25 23:30:34 -0500

    Disabled embedded metadata by default and cleaned up Mod Menu experience

--- common/src/main/java/com/milezerosoftware/mc/screenshotmanagerenhanced/config/ModConfig.java
+++ common/src/main/java/com/milezerosoftware/mc/screenshotmanagerenhanced/config/ModConfig.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 public class ModConfig {
     // Global Settings
-    public boolean embedMetadata = true; // Default to enabled
-    public boolean displayRelativePath = true;
+    public boolean embedMetadata = false; // Default to disabled
+    public boolean displayRelativePath = true; // Default to enabled
     public GroupingMode groupingMode = GroupingMode.WORLD; // Default to WORLD
 
     // Per-World Rules: Key = WorldName/IP, Value = Configuration
